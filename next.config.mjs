ab0a6b0e1ec4569b71f8169f48bb865aa22382e6
mnuralim <shadownur345@gmail.com>
2024-10-29 12:14:27 +0800

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/user',
+        permanent: true,
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
